aa2ab91d19f0d88c3477c699f91f0930275861e1
Oops19 <Oops19@users.noreply.github.com>
2024-06-07 00:36:01 +0200

    update

--- xml_injector/modinfo.py
+++ xml_injector/modinfo.py
@@ -8,7 +8,7 @@ from sims4communitylib.mod_support.common_mod_info import CommonModInfo
 
 
 class ModInfo(CommonModInfo):
-    """ Mod info for the S4.CL Sample Mod. """
+    """ Mod info for the S4CL Sample Mod. """
     # To create a Mod Identity for this mod, simply do ModInfo.get_identity(). Please refrain from using the ModInfo of The Sims 4 Community Library in your own mod and instead use yours!
     _FILE_PATH: str = str(__file__)
 
